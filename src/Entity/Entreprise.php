14af01db1c18fd57f5c29e0aa426720df774fd2a
Superhitya2 <superhitya2@gmail.com>
2018-12-23 17:57:19 +0000

    test de build

--- src/Entity/Entreprise.php
+++ src/Entity/Entreprise.php
@@ -150,7 +150,6 @@ class Entreprise
         return $this;
     }
 
-
     /**
      * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
      * of 'UploadedFile' is injected into this setter to trigger the update. If this
