0335a4b1430a02d80855aff843d8acef76c95083
Superhitya2 <superhitya2@gmail.com>
2019-01-10 17:05:16 +0000

    others nelmio config

--- public/index.php
+++ public/index.php
@@ -33,7 +33,7 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 }
 
 $kernel = new Kernel($env, $debug);
-Request::enableHttpMethodParameterOverride(); // <-- add this line
+//Request::enableHttpMethodParameterOverride(); // <-- add this line
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
