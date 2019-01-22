e51b82fd7d21599ed3f74b32f349e1abe24753e4
Superhitya2 <superhitya2@gmail.com>
2019-01-22 23:17:27 +0000

    set email confirmation listner

--- src/EventSubscriber/UserEmailConfirmationListener.php
+++ src/EventSubscriber/UserEmailConfirmationListener.php
@@ -1,21 +1,11 @@
 <?php
 
-/*
- * This file is part of the FOSUserBundle package.
- *
- * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App\EventSubscriber;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\User;
 use FOS\UserBundle\FOSUserEvents;
-//use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Mailer\Mailer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -43,9 +33,9 @@ class UserEmailConfirmationListener extends Controller implements EventSubscribe
      * @param SessionInterface        $session
      */
     //$mailer, 
-    public function __construct( TokenGeneratorInterface $tokenGenerator, UrlGeneratorInterface $router, SessionInterface $session)
+    public function __construct( Mailer $mailer,TokenGeneratorInterface $tokenGenerator, UrlGeneratorInterface $router, SessionInterface $session)
     {
-       // $this->mailer = $mailer;
+        $this->mailer = $mailer;
         $this->tokenGenerator = $tokenGenerator;
         $this->router = $router;
         $this->session = $session;
@@ -57,35 +47,25 @@ class UserEmailConfirmationListener extends Controller implements EventSubscribe
     public static function getSubscribedEvents()
     {
         return array(
-            //FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess',
             KernelEvents::VIEW => ['onRegistrationSuccess', EventPriorities::POST_WRITE],
 
         );
     }
 
-
     public function onRegistrationSuccess(GetResponseForControllerResultEvent $event)
     {
-        
-        // /** @var $user \FOS\UserBundle\Model\UserInterface */
-        //$user = $event->getForm()->getData();
-        
         $user = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
         if (!$user instanceof User || Request::METHOD_POST !== $method) {
             return;
         }
-        $user->setEnabled(true);
-        //$user->setEnabled(false);
+
         if (null === $user->getConfirmationToken()) {
             $user->setConfirmationToken($this->tokenGenerator->generateToken());
         }
-        
-        //$emailRequest = $this->get('fos_user.mailer.twig_swift');
-        //$emailRequest->sendConfirmationEmailMessage($user);
 
-        //$this->mailer->sendConfirmationEmailMessage($user);
+        $this->mailer->sendConfirmationEmailMessage($user);
 
         $this->session->set('fos_user_send_confirmation_email/email', $user->getEmail());
 
